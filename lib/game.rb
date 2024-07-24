adb5dd736c9606e5d752e8e75815695446f57691
Gustavo <gustavoooliveiradecarvalho@gmail.com>
2024-07-24 01:16:05 -0400

    feat: show the info of the play

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'player'
+require_relative 'secret_word'
 # Contains all the game logic including: sequence, verification and result display
 class Game
   attr_reader :secret_word, :player
@@ -8,22 +9,30 @@ class Game
   def initialize(player_name)
     @secret_word = SecretWord.new
     @player = Player.new(player_name)
+    @secret_word.random_word
   end
 
   def play
     until game_end?
-      # show info
+      show_info
       letter = player.guess
+      puts letter     # -- remove --
       case secret_word.contains? letter
       when true
         save_guess letter
       else # when false
-        player.tries_left = -1
+        player.minus_one
       end
     end
   end
 
   private
+
+  def show_info
+    puts "\nword: #{secret_word.display(player.guessed_letters)}"
+    puts "Tries left: #{player.tries_left}"
+    puts "Guessed letters: #{player.guessed_letters.join(', ')}"
+  end
 
   def save_guess(letter)
     player.add_at_guesses letter
@@ -36,6 +45,6 @@ class Game
   end
 
   def won?
-    player.guessed_letters.all? { |char| secret_word.contains? char }
+    secret_word.word.split('').all? { |c| player.guessed_letters.include? c }
   end
 end
