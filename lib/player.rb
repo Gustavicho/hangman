62c468de5b69ad418188adaedc387ef094ef0014
Gustavo <gustavoooliveiradecarvalho@gmail.com>
2024-07-24 00:04:16 -0400

    refactor: make more legible & concise
    - add docs

--- lib/player.rb
+++ lib/player.rb
@@ -2,47 +2,51 @@
 
 require_relative 'displayable'
 
-# Contains all methods to get the input from the user
+# Contains all methods to get the input from the user adn contains all that needs to play: secret_word, hist
 class Player
-  attr_reader :hist, :name
+  attr_reader :name, :guessed_letters
+  attr_accessor :tries_left
 
-  def initialize
-    @name = nil
-    @hist = []
-  end
+  include Displayable
 
-  def chose_name
-    loop do
-      print insert_name
-      name = gets.chomp
-      break if name.between? 3, 15
-
-      puts name_size_error
-    end
+  def initialize(name)
     @name = name
+    @tries_left = 8
+    @guessed_letters = []
   end
 
+  # get a input from the player
+  # only letter & ONE char long
+  # or the player will be in a infinite loop
   def guess
     loop do
-      print insert_here
-      play = gets.chomp.downcase
+      print guess_txt
+      letter = gets.chomp
+      return letter if valid? letter
 
-      return play if valid?(play)
-
-      puts inpt_size_error if play.size != 1
-      puts inpt_type_error unless play.between?('a', 'z')
+      puts guess_error
     end
   end
 
-  def save_in_hist(letter)
-    @hist << letter
+  # add a letter in the gue'ssed_letters
+  # BUT, raise a ArgumentError if the current letter has already been added
+  def add_at_guesses(letter)
+    raise ArgumentError, 'This letter has already been used' if letter_used? letter
+
+    @guessed_letters << letter
   end
 
   private
 
-  include Displayable
-
+  # check if the input is a char and a letter
+  # to only get CHAR's
   def valid?(input)
     input.size == 1 && input.between?('a', 'z')
+  end
+
+  # check if the letter already exist
+  # to avoid repeat letter
+  def letter_used?(letter)
+    @guessed_letters.include? letter
   end
 end
